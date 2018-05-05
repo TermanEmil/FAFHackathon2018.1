d2bf9123fbd784ed773eb73ef4b6c53d727fdf16
Emil Terman <terman.emil@gmail.com>
2018-05-05 16:38:24 +0300

    First push

---
+++ FAFHackathon/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float xVelocity = 10;
+    public float jumpVelocity = 300;
+
+    private float xInput;
+    private Rigidbody2D rb;
+
+	private void Start()
+	{
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update()
+	{
+        xInput = Input.GetAxis("Horizontal");
+	}
+
+	private void FixedUpdate()
+	{
+        rb.AddForce(Vector3.right * xInput * xVelocity * Time.fixedDeltaTime);
+	}
+}
